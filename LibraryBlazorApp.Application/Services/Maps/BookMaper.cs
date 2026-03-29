488342d7c302710255ad289566c6c9d180c98423
Rakun <szymonkonopnicki@outlook.com>
2026-03-29 20:33:57 +0200

    Add simple mapping class for book

--- LibraryBlazorApp.Application/Services/Maps/BookMaper.cs
+++ LibraryBlazorApp.Application/Services/Maps/BookMaper.cs
@@ -19,4 +19,57 @@ public static class BookMaper
             DeathDate = b.Author.DeathDate,
         }).ToList();
     }
+    public static IEnumerable<BookAdminDto> ToAdminDto(IEnumerable<Book> books)
+    {
+        return books.Select(b => new BookAdminDto
+        {
+            Id = b.Id,
+            BookCreatedAt = b.CreatedAt,
+            BookUpdatedAt = b.UpdatedAt,
+            BookStatus = b.GetAvailabilityStatus(),
+            Title = b.Title,
+            Description = b.Description,
+            Quantity = b.Quantity,
+            AuthorId = b.Author.Id,
+            AuthorCreatedAt = b.Author.CreatedAt,
+            AuthorName = b.Author.Name,
+            AuthorSurname = b.Author.Surname,
+            Bio = b.Author.Bio,
+            BirthDate = b.Author.BirthDate,
+            DeathDate = b.Author.DeathDate,
+            AuthorNationality = b.Author.Nationality,
+            IsAuthorAlive = b.Author.IsStillAlive()
+        }).ToList();
+    }
+    public static BookAdminDto ToAdminDto(Book book)
+    {
+        return new BookAdminDto()
+        {
+            Id = book.Id,
+            BookCreatedAt = book.CreatedAt,
+            BookUpdatedAt = book.UpdatedAt,
+            BookStatus = book.GetAvailabilityStatus(),
+            Title = book.Title,
+            Description = book.Description,
+            Quantity = book.Quantity,
+            AuthorId = book.Author.Id,
+            AuthorCreatedAt = book.Author.CreatedAt,
+            AuthorName = book.Author.Name,
+            AuthorSurname = book.Author.Surname,
+            Bio = book.Author.Bio,
+            BirthDate = book.Author.BirthDate,
+            DeathDate = book.Author.DeathDate,
+            AuthorNationality = book.Author.Nationality,
+            IsAuthorAlive = book.Author.IsStillAlive()
+        };
+    }
+    public static Book ToBook(BookEditDto book)
+    {
+        return new Book()
+        {
+            Title = book.Title,
+            Description = book.Description,
+            Quantity = book.Quantity
+        };
+    }
 }
