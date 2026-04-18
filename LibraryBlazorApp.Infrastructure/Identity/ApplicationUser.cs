67a6d103403c2db2c13e0cf0179c4aec9b1ac848
Rakun <szymonkonopnicki@outlook.com>
2026-04-18 12:57:26 +0200

    Add identity entity to infrastracure for separte layers

---
+++ LibraryBlazorApp.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using LibraryBlazorApp.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace LibraryBlazorApp.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public Client? Client { get; set; } 
+}
---
+++ LibraryBlazorApp.Infrastructure/Identity/LocalizedIdentityErrorDescriber.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LibraryBlazorApp.Infrastructure.Identity;
+
+public class LocalizedIdentityErrorDescriber : IdentityErrorDescriber
+{
+    private const string Password = "Password";
+    private const string Email = "Email";
+
+    public override IdentityError PasswordMismatch()
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "Password and Confirmed Password are different."
+        };
+    }
+
+    public override IdentityError PasswordRequiresDigit()
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "The password must contain at least 1 digit."
+        };
+    }
+
+    public override IdentityError PasswordRequiresLower()
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "The password must contain at least 1 lowercase letter."
+        };
+    }
+
+    public override IdentityError PasswordRequiresNonAlphanumeric()
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "The password must contain at least 1 special character (e.g. $, #, &, etc.)."
+        };
+    }
+
+    public override IdentityError PasswordRequiresUpper()
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "The password must contain at least 1 uppercase letter."
+        };
+    }
+
+    public override IdentityError PasswordTooShort(int length)
+    {
+        return new IdentityError
+        {
+            Code = Password,
+            Description = "The password must be at least 8 characters and no more than 100 characters."
+        };
+    }
+
+    public override IdentityError DuplicateUserName(string userName)
+    {
+        return new IdentityError
+        {
+            Code = Email,
+            Description = "The selected email is already taken."
+        };
+    }
+}
